954f0da6e82506bb8462f9810bb935f36935ca2e
Kenura R. Gunarathna <kenuragunarathna@gmail.com>
2026-01-15 18:45:13 +0530

    added improved putty print

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2026 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2026 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -22,9 +22,10 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include <string.h>
-#include <stdio.h>
 #include "usbd_cdc_if.h"
+#include <stdio.h>
+#include <string.h>
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -45,7 +46,14 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+#define CMD_BUF_SIZE 64
 
+char usb_cmd[CMD_BUF_SIZE];
+volatile uint8_t usb_cmd_ready = 0;
+volatile uint8_t counter_enabled =
+    0; // Accessible from CDC driver to suppress echo
+volatile uint8_t session_started =
+    0; // Flag to track if welcome banner was shown
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -58,15 +66,26 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+// Helper function to wait until USB is free
+void CDC_Send_Wait(const char *ptr, uint16_t len) {
+  uint32_t start = HAL_GetTick();
+  // Wait for USBD to be free or timeout after 10ms
+  extern USBD_HandleTypeDef hUsbDeviceFS;
+  USBD_CDC_HandleTypeDef *hcdc =
+      (USBD_CDC_HandleTypeDef *)hUsbDeviceFS.pClassData;
 
+  while (hcdc->TxState != 0 && (HAL_GetTick() - start < 10))
+    ;
+
+  CDC_Transmit_FS((uint8_t *)ptr, len);
+}
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
 
   /* USER CODE BEGIN 1 */
 
@@ -77,7 +96,8 @@ int main(void)
 
   /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
@@ -95,67 +115,100 @@ int main(void)
   MX_GPIO_Init();
   MX_USB_DEVICE_Init();
   /* USER CODE BEGIN 2 */
-  HAL_Delay(200);  // Wait for things to stabilize
-
-  // Hello World message
-  char msg[] = "Hello World from STM32H743!\r\n";
-  CDC_Transmit_FS((uint8_t*)msg, strlen(msg));
-  HAL_Delay(10);
-
-  // System info
-  char info[100];
-  sprintf(info, "System Clock: %lu MHz\r\n", HAL_RCC_GetSysClockFreq() / 1000000);
-  CDC_Transmit_FS((uint8_t*)info, strlen(info));
-  HAL_Delay(10);
-
-  sprintf(info, "HCLK: %lu MHz\r\n", HAL_RCC_GetHCLKFreq() / 1000000);
-  CDC_Transmit_FS((uint8_t*)info, strlen(info));
-  HAL_Delay(10);
-
-  char ready[] = "USB CDC Ready!\r\n";
-  CDC_Transmit_FS((uint8_t*)ready, strlen(ready));
-  HAL_Delay(10);
+  uint32_t counter = 0;
+  uint32_t last_tick = 0;
   /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  uint32_t counter = 0;
-  while (1)
-  {
-    /* USER CODE END WHILE */
+  while (1) {
+    if (usb_cmd_ready) {
+      usb_cmd_ready = 0;
 
-    /* USER CODE BEGIN 3 */
-	HAL_Delay(1000);  // 1 second delay
+      // Print welcome banner on first interaction
+      if (!session_started) {
+        session_started = 1;
+        const char *banner = "\r\n"
+                             "========================================\r\n"
+                             "   STM32 USB Serial Terminal v1.0\r\n"
+                             "========================================\r\n"
+                             "Commands: start, stop, status, exit\r\n"
+                             "Press Ctrl+C to stop counter\r\n"
+                             "========================================\r\n"
+                             "\r\n";
+        CDC_Send_Wait(banner, strlen(banner));
+        HAL_Delay(5);
+      }
 
-	char buffer[50];
-	sprintf(buffer, "Counter: %lu\r\n", counter++);
-	CDC_Transmit_FS((uint8_t*)buffer, strlen(buffer));
+      // 1. Check for Empty Command (User just pressed Enter)
+      if (strlen(usb_cmd) == 0) {
+        // User just hit Enter. ISR already printed \r\n.
+        // We just print the prompt again.
+        CDC_Transmit_FS((uint8_t *)"> ", 2);
+        continue;
+      }
+
+      if (strcmp(usb_cmd, "start") == 0) {
+        counter_enabled = 1;
+        CDC_Transmit_FS((uint8_t *)"Counter started\r\n", 17);
+      } else if (strcmp(usb_cmd, "stop") == 0) {
+        counter_enabled = 0;
+        CDC_Transmit_FS((uint8_t *)"Counter stopped\r\n", 17);
+      } else if (strcmp(usb_cmd, "exit") == 0) {
+        counter_enabled = 0;
+        CDC_Transmit_FS((uint8_t *)"Exited loop mode\r\n", 18);
+      } else if (strcmp(usb_cmd, "status") == 0) {
+        char msg[50];
+        sprintf(msg, "Counter=%lu, Enabled=%d\r\n", counter, counter_enabled);
+        CDC_Transmit_FS((uint8_t *)msg, strlen(msg));
+      } else {
+        CDC_Transmit_FS((uint8_t *)"Unknown command\r\n", 17);
+      }
+
+      // Clean buffer and reprint prompt
+      memset(usb_cmd, 0, sizeof(usb_cmd));
+
+      // Wait a tiny bit to ensure previous message sent (simple flow control)
+      HAL_Delay(2);
+
+      // Print the prompt for the next line
+      CDC_Transmit_FS((uint8_t *)"> ", 2);
+    }
+
+    /* ---- Non-blocking counter ---- */
+    if (counter_enabled && (HAL_GetTick() - last_tick >= 1000)) {
+      last_tick = HAL_GetTick();
+      counter++;
+
+      // Clear current line and print counter cleanly (no prompt)
+      char msg[64];
+      sprintf(msg, "\rCounter: %lu                    \r\n", counter);
+      CDC_Send_Wait(msg, strlen(msg));
+    }
   }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Supply configuration update enable
-  */
+   */
   HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE0);
 
-  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
+  while (!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {
+  }
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
@@ -168,16 +221,15 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_2;
   RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
   RCC_OscInitStruct.PLL.PLLFRACN = 0;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2
-                              |RCC_CLOCKTYPE_D3PCLK1|RCC_CLOCKTYPE_D1PCLK1;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2 |
+                                RCC_CLOCKTYPE_D3PCLK1 | RCC_CLOCKTYPE_D1PCLK1;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
@@ -186,19 +238,17 @@ void SystemClock_Config(void)
   RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2;
   RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
-  {
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK) {
     Error_Handler();
   }
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
   /* USER CODE BEGIN MX_GPIO_Init_1 */
 
   /* USER CODE END MX_GPIO_Init_1 */
@@ -216,17 +266,16 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE END 4 */
 
- /* MPU Configuration */
+/* MPU Configuration */
 
-void MPU_Config(void)
-{
+void MPU_Config(void) {
   MPU_Region_InitTypeDef MPU_InitStruct = {0};
 
   /* Disables the MPU */
   HAL_MPU_Disable();
 
   /** Initializes and configures the Region and the memory to be protected
-  */
+   */
   MPU_InitStruct.Enable = MPU_REGION_ENABLE;
   MPU_InitStruct.Number = MPU_REGION_NUMBER0;
   MPU_InitStruct.BaseAddress = 0x0;
@@ -242,24 +291,21 @@ void MPU_Config(void)
   HAL_MPU_ConfigRegion(&MPU_InitStruct);
   /* Enables the MPU */
   HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
-
 }
 
 /**
-  * @brief  Period elapsed callback in non blocking mode
-  * @note   This function is called  when TIM6 interrupt took place, inside
-  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
-  * a global variable "uwTick" used as application time base.
-  * @param  htim : TIM handle
-  * @retval None
-  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
-{
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM6 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
   /* USER CODE BEGIN Callback 0 */
 
   /* USER CODE END Callback 0 */
-  if (htim->Instance == TIM6)
-  {
+  if (htim->Instance == TIM6) {
     HAL_IncTick();
   }
   /* USER CODE BEGIN Callback 1 */
@@ -268,32 +314,30 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 }
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
-  while (1)
-  {
+  while (1) {
   }
   /* USER CODE END Error_Handler_Debug */
 }
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(uint8_t *file, uint32_t line) {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* User can add his own implementation to report the file name and line
+     number, ex: printf("Wrong parameters value: file %s on line %d\r\n", file,
+     line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
